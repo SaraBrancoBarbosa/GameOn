8960382b5bce320ffce5797b07ddc7b77b0c46b2
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-06-21 16:09:21 +0200

    Tests are successful

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,12 @@ function editNav() {
 /*********** Elements ***********/
 
 // DOM elements
-const modalbg = document.querySelector(".bground");
+const validationBg = document.querySelector(".bground");
+const modalContent = validationBg.querySelector(".content");
 const modalBtns = document.querySelectorAll(".modal-btn");
 const form = document.querySelector("form");
 const formData = document.querySelectorAll(".formData");
-const confirmationMsgBox = document.getElementById("confirmationMsgBox");
-const closeBtns = document.querySelectorAll(".close-btn");
+const confirmationBg = document.getElementById("confirmationBg");
 const submitBtn = document.querySelector(".btn-submit");
 const input = document.querySelectorAll(".input")
 
@@ -28,54 +28,67 @@ const email = document.getElementById("email");
 const birthdate = document.getElementById("birthdate");
 const tournamentQuantity = document.getElementById("quantity");
 const citiesBtnRadio = document.getElementsByName("location");
-/*
-Je n'arrive pas à rendre fonctionnel les btn radios en passant par citiesBtnRadio.
-J'aimerais cependant comprendre pourquoi cela fonctionne quand j'écris le code dans la fonction, mais pas ici
-Sous cette forme : document.querySelectorAll("input[name='location']")
-*/
 const tosChecked = document.getElementById("checkbox1");
 
 /*********** Launching and closing modal form ***********/
 
-// Launch modal form (function)
-function launchModal() {
-  modalbg.style.display = "block";
+// Function to prevent further propagation of the current event
+const stopPropagation = (event) => { 
+  event.stopPropagation();
+}
+
+// Function to launch the modals
+function launchModal(modal, closeFunction) {
+  modal.style.display = "block";
+  // "Hides" the background and removes its scrolling bar
+  document.body.style.overflow = "hidden"; 
+  // Clicking outside of the form closes it
+  modal.addEventListener("click", closeFunction); 
+  modal.querySelector(".content").addEventListener("click", stopPropagation);
+
+  const closeBtns = modal.querySelectorAll(".close-btn");
+  closeBtns.forEach((btn) => btn.addEventListener("click", closeFunction));
+}
+
+// Function to close the modals
+function closeModal(modal, closeFunction) {
+  modal.style.display = "none";
+  document.body.style.overflow = "auto";
+  modal.removeEventListener("click", closeFunction);
+  modal.querySelector(".content").removeEventListener("click", stopPropagation);
+}
+
+// Function to launch modal (form)
+function launchValidation() {
+  launchModal(validationBg, closeValidation);
   submitBtn.addEventListener("click", validate);
 }
 
-// Close modal form (function)
-function closeModal() {
-  modalbg.style.display = "none";
+// Function to launch modal (confirmation message)
+function launchConfirmation() {
+  launchModal(confirmationBg, closeConfirmation);
 }
 
-// Launch modal event
-modalBtns.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// Close modal event
-closeBtns.forEach((btn) => btn.addEventListener("click", closeModal));
-
-/*********** Launching and closing confirmation message ***********/
-
-// Launch modal confirmation message when form submitted (function)
-function launchConfirmMsg() {
-  event.preventDefault(); // J'aimerais pouvoir enlever cette ligne
-  confirmationMsgBox.style.display = "flex";
+// Function to close modal (form)
+function closeValidation() {
+  closeModal(validationBg, closeValidation);
+  submitBtn.removeEventListener("click", validate);
 }
 
-// Close modal confirmation message when form submitted (function)
-function closeConfirmMsg() {
-  confirmationMsgBox.style.display = "none";
+// Function to close modal (confirmation message)
+function closeConfirmation() {
+  closeModal(confirmationBg, closeConfirmation);
 }
 
-// Close modal confirmation message when form submitted
-closeBtns.forEach((btn) => btn.addEventListener("click", closeConfirmMsg));
+// Launches modal event by clicking on the corresponding buttons
+modalBtns.forEach((btn) => btn.addEventListener("click", launchValidation));
 
 /*********** Form elements functions ***********/
 
 /**
  * Fonction de validation
- * @param {HTMLElement} element La fonction reçoit la variable element
- * @returns 
+ * @param {HTMLElement} element
+ * @returns {boolean} True if element.value is valid
  */
 
 // First name and last name function
@@ -93,7 +106,7 @@ const validateName = (element) => {
     element.parentNode.dataset.error="Caractères invalides."
     element.parentNode.setAttribute("data-error-visible",true);
   }
-  return result
+  return result;
 }
 
 // Email function
@@ -101,40 +114,43 @@ const validateEmail = () => {
   let result = true;
   formData[2].setAttribute("data-error-visible", false);
 
-  if (!email.value.match(/^[a-z0-9._-]+@[a-z0-9._-]+.[a-z0-9._-]+$/)) {
+  if (!email.value.match(/^[a-z0-9._-]+@[a-z0-9._-]+.([a-z0-9._-]{2,})+$/)) {
     result = false;
     formData[2].setAttribute("data-error", "Veuillez entrer une adresse email valide.");
     formData[2].setAttribute("data-error-visible", true);
   }
-  return result
+  return result;
 }
-
-
 
 // Birthdate function
 const validateBirthdate = () => {
   let result = true;
   formData[3].setAttribute("data-error-visible", false);
   
-  // Birthdate: the user can't be a minor (en cours !)
+  // The user can't be a minor. We compare the valid minimum date with the user's birthdate
   const today = new Date();
   const validMinDate = new Date(
     today.getFullYear()-18,
     today.getMonth(),
     today.getDate(),
   );
+  const birth = new Date(birthdate.value);
+  const realBirthday = new Date(
+    birth.getFullYear(),
+    birth.getMonth(),
+    birth.getDate(),
+  );
 
   if (!birthdate.value.match(/^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/)) {
     result = false;
     formData[3].setAttribute("data-error", "Veuillez entrer votre date de naissance.");
     formData[3].setAttribute("data-error-visible", true);
-    /* Mettre info pour être majeur */
-   } else if (!birthdate < validMinDate) {
+  } else if (realBirthday.getTime() > validMinDate.getTime()) {
     result = false;
     formData[3].setAttribute("data-error-visible", true);
     formData[3].setAttribute("data-error", "Vous devez être majeur pour vous inscrire."); 
   }
-  return result
+  return result;
 }
 
 // Tournament quantity function
@@ -147,25 +163,27 @@ const validateTournamentQuantity = () => {
     formData[4].setAttribute("data-error", "Veuillez saisir un nombre entre 0 et 99.");
     formData[4].setAttribute("data-error-visible", true);
   }
-  return result
+  return result;
 }
 
-// Cities selection function
-// Code non fonctionnel
+// City selection function
 const validateCitiesBtnRadio = () => {
-  let result = true;
+  let result = false;
   formData[5].setAttribute("data-error-visible", false);
 
-  for (let i = 0; i < citiesBtnRadio.length; i++) {
+  for (let element of citiesBtnRadio) {
 
-    if (!citiesBtnRadio[i].checked) {
-      result = false;
-      formData[5].setAttribute("data-error", "Veuillez sélectionner une ville.");
-      formData[5].setAttribute("data-error-visible", true);
+    if (element.checked) {
+      result = true;
+      break;
     }
-    return result;
   }
-  
+  if (result === false) {
+  formData[5].setAttribute("data-error", "Veuillez sélectionner une ville.");
+  formData[5].setAttribute("data-error-visible", true);
+  }
+  return result;
+
 }
 
 // Terms of use function
@@ -178,12 +196,12 @@ const validateTos = () => {
     input[0].setAttribute("data-error", "Veuillez accepter les conditions d'utilisation.");
     input[0].setAttribute("data-error-visible", true);
   }
-  return result
+  return result;
 }
 
-/*********** Form validation ***********/
+/*********** Fields validation of the form ***********/
 
-function validate() {   // Code line 63 (HTML, onsubmit)
+function validateFields() {
 
   let result = true;
 
@@ -207,46 +225,21 @@ function validate() {   // Code line 63 (HTML, onsubmit)
  
     // Terms of use validation
     result = validateTos(tosChecked) && result;
-    // Doit-on faire quelque chose avec la case newsletter ?
 
-  // If all the datas are correct, the result is validated
+  // If all the fields are correct, the result is validated
   return result; 
 }
 
-/* Autre tentative pour raccourcir le code, mais les erreurs s'affichent une par une, pas toutes en même temps.
-
-function validate(event) {
-  event.preventDefault()
-
-    let result = true;
-
-    if (!(
-      (result = validateName(firstName) && result) 
-      && (result = validateName(lastName) && result) 
-      && (result = validateEmail(email) && result) 
-      && (result = validateBirthdate(birthdate) && result)
-      && (result = validateTournamentQuantity(tournamentQuantity) && result) 
-      && (result = validateCitiesBtnRadio(citiesBtnRadio) && result) 
-      && (result = validateTos(tosChecked) && result)
-    )) {
-      console.log("C'est pas bon è_é")
-    } else {
-      console.log("C'est bon :D")
-      launchConfirmMsg();
-    }
-}
-*/
-
 /*********** Form submission ***********/
-form.addEventListener("submit", (event) => {
-  // If the function is wrong, we keep the datas so the user can correct them. We prevent the page from loading.
-if (!validate(event)) {
+const validate = (event) => {
+  
+  // If the function is wrong, we keep the datas so the user can correct them. We prevent the page from loading
   event.preventDefault();
-  console.log("C'est pas bon è_é")
+  if (!validateFields(event)) {
   } else {
-    // The submission is validated
-    console.log("C'est bon :D")
-    closeModal();
-    launchConfirmMsg();  
+    // The submission is validated and the confirmation message is launched
+    closeValidation();
+    launchConfirmation();  
   }
-});
+
+};
